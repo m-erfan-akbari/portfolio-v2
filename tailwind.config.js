77dbae65aaff09f26130a5987856a646d39ba6cc
Mohammad Erfan Akbari <m.erfan.ak@gmail.com>
2025-01-12 20:14:04 +0330

    Add animation tailwind's config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     screens: {
-      xl: { max: "1200px" },
-      lg: { max: "979px" },
-      md: { max: "768px" },
-      sm: { max: "480px" },
+      xl: { max: '1200px' },
+      lg: { max: '979px' },
+      md: { max: '768px' },
+      sm: { max: '480px' },
       portrait: {
-        raw: "(orientation: portrait)",
+        raw: '(orientation: portrait)',
       },
       landscape: {
-        raw: "(orientation: landscape)",
+        raw: '(orientation: landscape)',
+      },
+    },
+    extend: {
+      keyframes: {
+        bounceArrow: {
+          '0%, 20%, 50%, 80%, 100%': {
+            transform: 'translateY(0) rotate(-45deg)',
+          },
+          '40%': { transform: 'translateY(10px) rotate(-45deg)' },
+          '60%': { transform: 'translateY(5px) rotate(-45deg)' },
+        },
+        fadeText: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0.5' },
+        },
+      },
+      animation: {
+        bounce: 'bounceArrow 2s infinite',
+        fade: 'fadeText 2s infinite',
       },
     },
   },
   plugins: [],
-};
+}
